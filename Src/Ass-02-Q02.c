72cf2adf4b17d4c00ea1ce70e8067d119ddc2932
Jordan Haigh <c3256730@uon.edu.au>
2018-04-28 13:19:13 +1000

    Working on analysis of input

--- Src/Ass-02-Q02.c
+++ Src/Ass-02-Q02.c
@@ -15,52 +15,137 @@ typedef struct {
 }Button;
 
 static Button buttons[20];
-char *textArray[] = {"7","8","9","+","-","4","5","6","/","x","1","2","3","sqrt","mod","0",".","clr","pow","="};
-char *inputString[];
-char *outputString[];
-int buttonHere(int x, int y, Button button)
+char *textArray[] = {"7","8","9","+","-","4","5","6","/","x","1","2","3","sqrt","+-","0",".","clr","pow","="};
+
+
+int buttonHere(int x, int y, Button button);
+Button buildButton(int x, int y, int w, int h, int id);
+void showButton(Button button);
+void buttonToString(Button button);
+void CalculatorInit(void);
+void CalculatorProcess(void);
+void analyseTouch(Button currentButtonPressed);
+
+
+void analyseTouch(Button currentButtonPressed)
 {
-	//returns the button id or -1
+	//0,1,2,  ,  , 5,6,7,  ,  ,10,11,12,  ,  ,  15,  ,  ,  ,  ,
 
-	if(button.startX  < x &&
-			x < button.startX + button.width &&
-			button.startY  < y &&
-			y < button.startY + button.height)
-		return button.id;
-	else
-		return -1;
+	if(currentButtonPressed.id == 0 ||  //7
+			currentButtonPressed.id == 1 || //8
+			currentButtonPressed.id == 2 || //9
+			currentButtonPressed.id == 5 || //4
+			currentButtonPressed.id == 6 || //5
+			currentButtonPressed.id == 7 ||  //6
+			currentButtonPressed.id == 10 ||//1
+			currentButtonPressed.id == 11 ||//2
+			currentButtonPressed.id == 12 )//3
+	{
+		buildInputString(currentButtonPressed.text);
+	}
+	else if(currentButtonPressed.id == 15) //0 -- Check that 0 is not the first char in the input string. otherwise
+	{
+		buildInputString(currentButtonPressed.text);
+	}
+	else if(currentButtonPressed.id == 16) //decimal point
+	{
+		buildInputString(currentButtonPressed.text);
+	}
+
+
+
+
+	//Check if it is equals, div or times first
+	//display error message saying that there isnt any numbers yet
+
+	//if 0-9, clr, +-
+
+
+
 }
 
-Button buildButton(int x , int y, int w, int h, int id){
-	Button b;
-	b.startX = x;
-	b.startY = y;
-	b.width = w;
-	b.height = h;
-	b.id = id;
-	//b.text = "";
-
-	b.text = textArray[id];
-
-	return b;
-}
-
-void showButton(Button button){
-	BSP_LCD_DrawRect(button.startX, button.startY, button.width, button.height);
-	BSP_LCD_DisplayStringAt(button.startX + button.width/2,button.startY + button.height/2,button.text,LEFT_MODE);
-
-}
-
-void buttonToString(Button button)
+static char *inputString = "0";
+static char *outputString;
+int stringLength = 1;
+void generateInputString(char *str)
 {
-	printf("Debug: \n");
-	printf("Button startX: %d\n", button.startX);
-	printf("Button startY: %d\n", button.startY);
-	printf("Button width: %d\n", button.width);
-	printf("Button height: %d\n", button.height);
-	printf("Button text: %s\n", button.text);
-	printf("Button id: %d\n", button.id);
+	if(strcmp(str, inputString) == 0)
+	{
+		//when the input string contains only a zero and the string trying to be appended is also 0
+		//do nothing, its still zero
+		return;
+	}
+	if(strcmp(str, ".") == 0)
+	{
+		//append decimal point to next position of string array
+		inputString = (char*)realloc(inputString, sizeof(inputString)+1);
+		//
+	}
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+char* newString;
+static int buildInputStringFirstTime = 0;
+static int stringIndex = 0;
+
+void buildInputString(char *c){
+	if(buildInputStringFirstTime == 1)	  {
+	  newString = (char*)malloc(strlen(c)); //start of string. will have reallocation of memory if string is longer
+	  buildInputStringFirstTime = 0; //now not the first run through
+	  if(c == '=') {
+		  newString[stringIndex] = '\0';
+
+		  wordCount = string_parser(newString, &array_of_words);
+
+		  //printArrayOfWords();
+		  //printf("DEBUG: word count -> %d", wordCount);
+		  analyseKeywords();
+		  freeEverything();
+
+		  //printNewString();
+		  buildInputStringFirstTime = 1; //reset
+	  }
+	  else{
+		  newString[stringIndex] = c;
+		  stringIndex++;
+	  }
+
+	}
+	else{
+		newString = (char*)realloc(newString, sizeof(newString)+1);
+
+		if(c == '\0' || c == 10){
+		  newString[stringIndex] = '\0';
+
+		  wordCount = string_parser(newString, &array_of_words);
+		  if(debugOn == 1) printArrayOfWords();
+		  analyseKeywords();
+		  freeEverything();
+
+
+		  buildInputStringFirstTime = 1; //reset
+		  stringIndex = 0; //reset
+
+		}
+		else{
+		  newString[stringIndex] = c;
+		  stringIndex++;
+		}
+	}
+}
+
 
 
 void CalculatorInit(void)
@@ -99,39 +184,14 @@ void CalculatorInit(void)
 //  // Create colour choices
 //  BSP_LCD_SetTextColor(LCD_COLOR_RED);
 //  BSP_LCD_FillRect(5, 200, 30, 30);
-//  BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
-//  BSP_LCD_FillRect(40, 200, 30, 30);
-//  BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-//  BSP_LCD_FillRect(75, 200, 30, 30);
-//  BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-//  BSP_LCD_FillRect(110, 200, 30, 30);
-//  BSP_LCD_SetTextColor(LCD_COLOR_MAGENTA);
-//  BSP_LCD_FillRect(145, 200, 30, 30);
-//  BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
-//  BSP_LCD_FillRect(180, 200, 30, 30);
-//  BSP_LCD_SetTextColor(LCD_COLOR_CYAN);
-//  BSP_LCD_FillRect(215, 200, 30, 30);
-//  BSP_LCD_SetTextColor(LCD_COLOR_YELLOW);
-//  BSP_LCD_FillRect(250, 200, 30, 30);
-//
 //  BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
 //  BSP_LCD_SetFont(&Font24);
 //  BSP_LCD_DisplayChar(290, 205, 'C');
 //  BSP_LCD_DrawHLine(  0, 196, 320);
-//
 //  BSP_LCD_DrawVLine(  1, 198,  35);
-//  BSP_LCD_DrawVLine( 37, 198,  35);
-//  BSP_LCD_DrawVLine( 72, 198,  35);
-//  BSP_LCD_DrawVLine(107, 198,  35);
-//  BSP_LCD_DrawVLine(142, 198,  35);
-//
-//  BSP_LCD_DrawVLine(177, 198,  35);
-//  BSP_LCD_DrawVLine(212, 198,  35);
-//  BSP_LCD_DrawVLine(247, 198,  35);
-//  BSP_LCD_DrawVLine(282, 198,  35);
-//  BSP_LCD_DrawVLine(317, 198,  35);
-//
-//  BSP_LCD_DrawHLine(  1, 232, 320);
+//	BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
+
+
 }
 
 
@@ -140,8 +200,6 @@ void CalculatorProcess(void)
 {
   // STEPIEN: Assume horizontal display
   //uint16_t linenum = 0;
-
-
 
   static uint8_t fingerTouching = 0;
   static uint8_t bounce = 0;
@@ -173,10 +231,13 @@ void CalculatorProcess(void)
 			  }
 		  }
 
-
-
 		  printf("I am touching the '%s'. ID is %d \n" , currentButtonPressed.text, currentButtonPressed.id);
 	      printf("TOUCH:  Got (%3d,%3d)\n", display.x, display.y);
+
+
+	      analyseTouch(currentButtonPressed);
+
+
 
 
 
@@ -195,82 +256,49 @@ void CalculatorProcess(void)
 	  }
   }
 
-  //todo implement debouncing
-  //fucking forloops dont work
+  //todo implement debouncing - busy waiting doesnt fucking work
 }
 
-//  else if(BSP_TP_GetDisplayPoint(&display) == 1)
-//  {
-//	  //Finger is not touching board
-//	  fingerTouching = 0;
-//	  if(fingerTouching == 1)
-//	  {
-//		//may possibly mean that finger is still on board
-//		  printf("finger off..\n");
-//		  bounce = 0;
-//	  }
-//  }
 
+int buttonHere(int x, int y, Button button)
+{
+	//returns the button id or -1
+	if(button.startX  < x &&
+			x < button.startX + button.width &&
+			button.startY  < y &&
+			y < button.startY + button.height)
+		return button.id;
+	else
+		return -1;
+}
 
+Button buildButton(int x , int y, int w, int h, int id){
+	Button b;
+	b.startX = x;
+	b.startY = y;
+	b.width = w;
+	b.height = h;
+	b.id = id;
 
+	b.text = textArray[id];
 
-  // getDisplayPoint(&display, Read_Ads7846(), &matrix );
-//  if (BSP_TP_GetDisplayPoint(&display) == 0) //0 means valid display point
-//  {
-//	  if(((display.y < 190) && (display.y >= 2)))
-//	  {
-//		if((display.x >= 318) || (display.x < 2))
-//		{
-//
-//		}
-//		else
-//		{
-//		  BSP_LCD_FillCircle(display.x, display.y, 2);
-//		  printf("TOUCH:  Got (%3d,%3d)\n", display.x, display.y);
-//		}
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 180) && (display.x <= 210))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_ORANGE);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 215) && (display.x <= 245))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_CYAN);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 250) && (display.x <= 280))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_YELLOW);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 5) && (display.x <= 35))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_RED);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 40) && (display.x <= 70))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 75) && (display.x <= 105))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 110) && (display.x <= 140))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 145) && (display.x <= 175))
-//	  {
-//		BSP_LCD_SetTextColor(LCD_COLOR_MAGENTA);
-//	  }
-//	  else if ((display.y <= 230) && (display.y >= 190) && (display.x >= 285) && (display.x <= 315))
-//	  {
-//		BSP_LCD_SetFont(&Font8);
-//		for(linenum = 0; linenum < 24; linenum++)
-//		{
-//		  BSP_LCD_ClearStringLine(linenum);
-//		}
-//	  }
-//	  else
-//	  {
-//	  }
-//  }
-//}
+	return b;
+}
+
+void showButton(Button button){
+	BSP_LCD_DrawRect(button.startX, button.startY, button.width, button.height);
+	BSP_LCD_DisplayStringAt(button.startX + button.width/2,button.startY + button.height/2,button.text,LEFT_MODE);
+
+}
+
+void buttonToString(Button button)
+{
+	printf("Debug: \n");
+	printf("Button startX: %d\n", button.startX);
+	printf("Button startY: %d\n", button.startY);
+	printf("Button width: %d\n", button.width);
+	printf("Button height: %d\n", button.height);
+	printf("Button text: %s\n", button.text);
+	printf("Button id: %d\n", button.id);
+}
+
